7c9827ae2015faf24715fc2286361d8078c55512
eframeart <47872462+eframeart@users.noreply.github.com>
2020-05-15 23:15:28 -0300

    Update show_courses_spec.rb

--- spec/requests/sections/show_courses_spec.rb
+++ spec/requests/sections/show_courses_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'GET /sections/3', type: :request do
 
   let!(:section) do
     Section.create!(
-      name: 'Pangeas Plus',
+      name: 'Cursos Pangeas',
       description: 'some section description',
       image_url: 'http://image-url'
     )
